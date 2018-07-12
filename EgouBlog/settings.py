edb94736cebf117c26f70be1fd4fd6ea5ee853a4
HuangHei <330202725@qq.com>
2018-07-13 00:54:20 +0800

    2018年7月13日00:54:19

--- EgouBlog/settings.py
+++ EgouBlog/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = '9%%f+swemuc-fq+^!z!#3)2o8or7o4yt!x4ick0o_n0_4_0o+r'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.awanmo.com','127.0.0.1']
 
